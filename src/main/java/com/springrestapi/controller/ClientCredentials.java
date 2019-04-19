17baddebadf7fcaf87034c2a1af891bc52239bbb
Karan Shah <karanshah197@gmail.com>
2019-04-19 18:34:45 -0400

    Update ClientCredentials.java

--- src/main/java/com/springrestapi/controller/ClientCredentials.java
+++ src/main/java/com/springrestapi/controller/ClientCredentials.java
@@ -3,7 +3,7 @@ package com.springrestapi.controller;
 public class ClientCredentials {
 
 	  /** Value of the "API key" shown under "Simple API Access". */
-	  static final String API_KEY = "AIzaSyCQlU-yynFwfGUMFmp2zhKz2hyage9vjWM";
+	  static final String API_KEY = "YOUR GOOGLE BOOK KEY";
 
 	  static void errorIfNotSpecified() {
 	    if (API_KEY.startsWith("Enter ")) {
